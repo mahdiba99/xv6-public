544472fe2b697747622ecf2a324f00842607530d
mahdiba99 <ali@pc.com>
2020-07-08 22:34:01 +0430

    define waitx function and initialize attrs

--- proc.c
+++ proc.c
@@ -112,6 +112,11 @@ found:
   memset(p->context, 0, sizeof *p->context);
   p->context->eip = (uint)forkret;
 
+  // handly initial proccess times
+  p->stime = ticks;
+  p->iotime = 0;
+  p->rtime = 0;
+  p->etime = 0;
   return p;
 }
 
@@ -260,7 +265,7 @@ exit(void)
         wakeup1(initproc);
     }
   }
-
+  curproc-> etime = ticks;
   // Jump into the scheduler, never to return.
   curproc->state = ZOMBIE;
   sched();
@@ -530,5 +535,50 @@ procdump(void)
         cprintf(" %p", pc[i]);
     }
     cprintf("\n");
+  }
+}
+
+int
+waitx(int *wtime, int *rtime){
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // Found one.(edited) 
+        cprintf("pid=%d, stime=%d, rtime=%d, iotime=%d, etime=%d, ticks=%d\n", p->pid, p->stime, p->rtime, p->iotime, p->etime, ticks);
+        *rtime = p->rtime;
+        *wtime = p->etime - p->stime - p->rtime - p->iotime;
+        
+
+        pid = p->pid;
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
   }
 }
